eb7c7a70d8b01202c03a0015da3688ad8ad7fa3f
gittadarling <nvieen69@gmail.com>
2021-06-11 23:38:16 +0200

    COMMENT main4

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ io.on('connection', function(socket) {
    })});
 
 http.listen(3000, function() {
-   console.log('listening on localhost:3000');});
+   console.log('listening on localhost:80');});
 
